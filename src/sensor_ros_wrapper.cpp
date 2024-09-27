ebe2a44f73c99aad24e2d224d23e355831b6e1e8
9woods123 <2726914442@qq.com>
2024-09-27 11:08:45 +0800

    new

--- src/sensor_ros_wrapper.cpp
+++ src/sensor_ros_wrapper.cpp
@@ -1,199 +1,111 @@
-// #include <ros/ros.h>
-// #include <sensor_msgs/PointCloud2.h>
-// #include <pcl_ros/point_cloud.h>
-// #include <pcl_conversions/pcl_conversions.h>
-// #include <pcl/point_cloud.h>
-// #include <pcl/point_types.h>
-// #include <vector>
-// #include <iostream>
-// #include <netinet/in.h>
-// #include <arpa/inet.h>
-
-// #define UDP_PORT 14556
-// #define NUM_FLOATS_PER_VECTOR 3 // 只包含 x, y, z
-
-
-
-// int main(int argc, char** argv) {
-//     ros::init(argc, argv, "point_cloud_publisher");
-//     ros::NodeHandle nh;
-//     ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud2>("point_cloud", 10);
-
-//     // 设置 UDP 套接字
-//     int sockfd;
-//     struct sockaddr_in serv_addr, cli_addr;
-//     socklen_t clilen = sizeof(cli_addr);
-//     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-//     if (sockfd < 0) {
-//         ROS_ERROR("ERROR opening socket");
-//         return -1;
-//     }
-
-//     bzero((char*)&serv_addr, sizeof(serv_addr));
-//     serv_addr.sin_family = AF_INET;
-//     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-//     serv_addr.sin_port = htons(UDP_PORT);
-//     if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
-//         ROS_ERROR("ERROR on binding");
-//         return -1;
-//     }
-
-
-//     while (ros::ok()) {
-//         // 接收 UDP 数据
-//         char buffer[1024 * 100];
-//         int n = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&cli_addr, &clilen);
-//         if (n < 0) {
-//             ROS_ERROR("ERROR in recvfrom");
-//             continue;
-//         }
-
-//         // 解析 x, y, z 数据
-//         int num_points = n / (NUM_FLOATS_PER_VECTOR * sizeof(float));
-//         std::cout<<"num_points: "<<num_points<<std::endl;
-        
-//         sensor_msgs::PointCloud2 msg;
-//         msg.header.frame_id = "drone";
-//         msg.header.stamp = ros::Time::now();
-
-//         // 填充消息字段
-//         const uint32_t POINT_STEP = 12; // 每个点 3 个 float (x, y, z)
-//         msg.fields.resize(3);
-//         msg.fields[0].name = "x";
-//         msg.fields[0].offset = 0;
-//         msg.fields[0].datatype = sensor_msgs::PointField::FLOAT32;
-//         msg.fields[0].count = 1;
-//         msg.fields[1].name = "y";
-//         msg.fields[1].offset = 4;
-//         msg.fields[1].datatype = sensor_msgs::PointField::FLOAT32;
-//         msg.fields[1].count = 1;
-//         msg.fields[2].name = "z";
-//         msg.fields[2].offset = 8;
-//         msg.fields[2].datatype = sensor_msgs::PointField::FLOAT32;
-//         msg.fields[2].count = 1;
-//         msg.data.resize(num_points * POINT_STEP);
-
-//         uint8_t *ptr = msg.data.data();
-
-        
-//         for (int i = 0; i < num_points; ++i) {
-//             float x, y, z;
-//             memcpy(&x, &buffer[i * NUM_FLOATS_PER_VECTOR * sizeof(float)], sizeof(float));
-//             memcpy(&y, &buffer[i * NUM_FLOATS_PER_VECTOR * sizeof(float) + sizeof(float)], sizeof(float));
-//             memcpy(&z, &buffer[i * NUM_FLOATS_PER_VECTOR * sizeof(float) + 2 * sizeof(float)], sizeof(float));
-
-//             *((float*)(ptr + 0)) = x; // x C++ 中，float 类型的标准大小通常是 4 字节。
-//             *((float*)(ptr + 4)) = -y; // y  ue中是左手系
-//             *((float*)(ptr + 8)) = z; // z
-//             ptr += POINT_STEP;
-//         }
-
-//         msg.point_step = POINT_STEP;
-//         msg.is_bigendian = false;
-//         msg.width = msg.data.size() / POINT_STEP;
-//         msg.height = 1;
-//         msg.row_step = msg.data.size();
-//         msg.is_dense = true;
-
-//         // 发布点云消息
-//         pub.publish(msg);
-//     }
-
-//     return 0;
-// }
-
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
-#include <sensor_msgs/point_cloud2_iterator.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <boost/asio.hpp>
+#include <sensor_msgs/point_cloud2_iterator.h>
 
-using boost::asio::ip::udp;
+#define UDP_PORT 14556
+#define NUM_FLOATS_PER_VECTOR 3 // 每个点只包含 x, y, z
 
-
-class PointCloudReceiver {
+class PointCloudPublisher {
 public:
-    PointCloudReceiver(ros::NodeHandle& nh) 
-        : socket_(io_service_, udp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 14556)) // 
-    {
-        pointcloud_pub_ = nh.advertise<sensor_msgs::PointCloud2>("/point_cloud", 10);
-        start_receive();
-
+    PointCloudPublisher(ros::NodeHandle& nh)
+        : pub(nh.advertise<sensor_msgs::PointCloud2>("point_cloud", 10)) {
+        setup_socket();
     }
 
     void run() {
-        io_service_.run(); // 启动事件循环
+        while (ros::ok()) {
+            receive_data();
+        }
     }
 
 private:
-    void start_receive() {
-        socket_.async_receive_from(
-            boost::asio::buffer(receive_buffer_), sender_endpoint_,
-            boost::bind(&PointCloudReceiver::handle_receive, this,
-                        boost::asio::placeholders::error,
-                        boost::asio::placeholders::bytes_transferred));
-    }
+    ros::Publisher pub;
+    int sockfd;
+    struct sockaddr_in serv_addr, cli_addr;
+    socklen_t clilen = sizeof(cli_addr);
 
-    void handle_receive(const boost::system::error_code& error, std::size_t bytes_transferred) {
-
-        if (!error) {
-            parse_point_cloud(receive_buffer_, bytes_transferred); // 使用数组名
-            start_receive(); // 继续接收
+    void setup_socket() {
+        // 创建套接字
+        sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+        if (sockfd < 0) {
+            ROS_ERROR("ERROR opening socket");
+            exit(-1);
         }
 
+        // 初始化服务器地址结构
+        bzero((char*)&serv_addr, sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        serv_addr.sin_port = htons(UDP_PORT);
 
+        // 绑定套接字
+        if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+            ROS_ERROR("ERROR on binding");
+            exit(-1);
+        }
     }
 
-    void parse_point_cloud(const uint8_t* data, std::size_t length) {
+    void receive_data() {
+        char buffer[1024 * 100]; // 接收缓冲区
+        int n = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&cli_addr, &clilen);
+        if (n < 0) {
+            ROS_ERROR("ERROR in recvfrom");
+            return;
+        }
 
-        // 假设每个点由3个浮点数（x, y, z）组成
-        size_t num_points = length / (3 * sizeof(float));
+        publish_point_cloud(buffer, n);
+    }
 
-        sensor_msgs::PointCloud2 pointcloud_msg;
-        pointcloud_msg.header.stamp = ros::Time::now();
-        pointcloud_msg.header.frame_id = "lidar"; // 设置坐标系
-        pointcloud_msg.height = 1; // 一维点云
-        pointcloud_msg.width = num_points;
-        pointcloud_msg.is_dense = true; // 无效点云
+    void publish_point_cloud(const char* data, int length) {
+        int num_points = length / (NUM_FLOATS_PER_VECTOR * sizeof(float));
 
-        // 设置点云的字段
-        sensor_msgs::PointCloud2Modifier modifier(pointcloud_msg);
+        sensor_msgs::PointCloud2 msg;
+        msg.header.frame_id = "lidar"; // 设置坐标系
+        msg.header.stamp = ros::Time::now();
+        msg.fields.resize(3);
+        setup_fields(msg);
+
+        msg.data.resize(num_points * msg.point_step);
+        float* ptr = reinterpret_cast<float*>(msg.data.data());
+
+        // 复制数据到消息
+        const float* received_data = reinterpret_cast<const float*>(data);
+        for (int i = 0; i < num_points; ++i) {
+            ptr[i * 3]     = received_data[i * 3];         // x
+            ptr[i * 3 + 1] = -received_data[i * 3 + 1]; // y (UE4使用左手坐标系)
+            ptr[i * 3 + 2] = received_data[i * 3 + 2]; // z
+        }
+
+        msg.point_step = sizeof(float) * 3;
+        msg.width = num_points;
+        msg.height = 1;
+        msg.row_step = msg.point_step * num_points;
+        msg.is_dense = true;
+
+        pub.publish(msg);
+    }
+
+    void setup_fields(sensor_msgs::PointCloud2& msg) {
+
+        sensor_msgs::PointCloud2Modifier modifier(msg);
         modifier.setPointCloud2Fields(3, 
             "x", 1, sensor_msgs::PointField::FLOAT32,
             "y", 1, sensor_msgs::PointField::FLOAT32,
             "z", 1, sensor_msgs::PointField::FLOAT32
         );
-
-        // 分配内存
-        pointcloud_msg.data.resize(num_points * sizeof(float) * 3);
-        pointcloud_msg.point_step = sizeof(float) * 3; // 每个点的字节数
-        pointcloud_msg.row_step = pointcloud_msg.point_step * pointcloud_msg.width; // 行步长
-
-        // 将数据复制到 PointCloud2 消息中
-        float* point_data = reinterpret_cast<float*>(pointcloud_msg.data.data());
-        const float* received_data = reinterpret_cast<const float*>(data);
-        for (size_t i = 0; i < num_points; ++i) {
-            point_data[i * 3]     = received_data[i * 3];     // x
-            point_data[i * 3 + 1] = -received_data[i * 3 + 1]; // y 需要根据你的坐标系调整
-            point_data[i * 3 + 2] = received_data[i * 3 + 2]; // z
-        }
-
-        pointcloud_pub_.publish(pointcloud_msg);
-        
     }
-
-    boost::asio::io_service io_service_;
-    udp::socket socket_;
-    udp::endpoint sender_endpoint_;
-    uint8_t receive_buffer_[4096]; // 接收缓冲区
-    ros::Publisher pointcloud_pub_;
 };
 
+
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "udp_point_cloud_receiver");
+    ros::init(argc, argv, "point_cloud_publisher");
     ros::NodeHandle nh;
 
-    PointCloudReceiver receiver(nh);
-    receiver.run(); // 启动事件循环
-    ros::spin();
+    PointCloudPublisher publisher(nh);
+    publisher.run();
+
     return 0;
 }
